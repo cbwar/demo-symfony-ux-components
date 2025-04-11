b9bbcd3b16d27c0c601ba21841e600a130fdd563
Raphael Lisch <cbwar@protonmail.com>
2025-04-11 23:58:26 +0200

    add live event

--- src/Twig/Components/AddToCartButton.php
+++ src/Twig/Components/AddToCartButton.php
@@ -6,12 +6,14 @@ use App\Services\Cart;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentToolsTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
 final class AddToCartButton
 {
     use DefaultActionTrait;
+    use ComponentToolsTrait;
 
     #[LiveProp]
     public int $productId;
@@ -29,11 +31,13 @@ final class AddToCartButton
     public function add(): void
     {
         $this->cart->addItem($this->productId);
+        $this->emit('cartUpdated');
     }
 
     #[LiveAction]
     public function remove(): void
     {
         $this->cart->removeItem($this->productId);
+        $this->emit('cartUpdated');
     }
 }
--- src/Twig/Components/CartButton.php
+++ src/Twig/Components/CartButton.php
@@ -4,6 +4,7 @@ namespace App\Twig\Components;
 
 use App\Services\Cart;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveListener;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
@@ -11,12 +12,21 @@ final class CartButton
 {
     use DefaultActionTrait;
 
+    private int $productCount = 0;
+
     public function __construct(private Cart $cart)
     {
+        $this->updateProductCount();
+    }
+
+    #[LiveListener('cartUpdated')]
+    public function updateProductCount(): void
+    {
+        $this->productCount = $this->cart->count();
     }
 
     public function getItemCount(): int
     {
-        return $this->cart->count();
+        return $this->productCount;
     }
 }
